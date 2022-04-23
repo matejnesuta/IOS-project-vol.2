e994dd402a658cdba0e42d9273f27a30266f92cd
matejnesuta <matejnetusa@gmail.com>
2022-04-23 22:38:49 +0200

    vidlickovani

--- proj2.c
+++ proj2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
 #define TI 1000
 #define TB 1000 
 
@@ -17,7 +22,9 @@ void errors(int errNum)
     char messages[3][50] = {
         "Byl zadan nespravny pocet argumentu.",
         "Na vstupu nebyl zadan celocisleny parametr.",
-        "Byly zadany argumenty v nespravnem rozsahu."};
+        "Byly zadany argumenty v nespravnem rozsahu.",
+        "Nepodarilo se rozdelit procesy."
+    };
 
     fprintf(stderr, "%s\n", messages[errNum - 1]);
     exit(1);
@@ -50,10 +57,41 @@ int paramControl(int argc, char *argv[], parameters *params)
     return 0;
 }
 
+void oxygen(){
+    printf("oxygen\n");
+    exit(0);
+}
+
+void hydrogen(){
+    printf("hydrogen\n");
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
     parameters params;
     paramControl(argc, argv, &params);
-    printf("%d %d %d %d\n", params.no, params.nh, params.ti, params.tb);
+    int *m = mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    *m = 10;
+    
+    for (int i=0; i<params.no;i++){
+        pid_t process = fork();
+        if (process == 0){
+            oxygen();
+        }
+        else if (process == -1){
+            errors(4);
+        }
+    }
+
+    for (int i=0; i<params.nh;i++){
+        pid_t process = fork();
+        if (process == 0){
+            hydrogen();
+        }
+        else if (process == -1){
+            errors(4);
+        }
+    }
     return 0;
 }
