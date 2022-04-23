dc4298e16d7ce652538ed035e8206cfd55741c1c
matejnesuta <matejnetusa@gmail.com>
2022-04-23 22:39:28 +0200

    refraktorizace kodu

--- proj2.c
+++ proj2.c
@@ -6,8 +6,7 @@
 #include <sys/stat.h>
 
 #define TI 1000
-#define TB 1000 
-
+#define TB 1000
 
 typedef struct
 {
@@ -23,8 +22,7 @@ void errors(int errNum)
         "Byl zadan nespravny pocet argumentu.",
         "Na vstupu nebyl zadan celocisleny parametr.",
         "Byly zadany argumenty v nespravnem rozsahu.",
-        "Nepodarilo se rozdelit procesy."
-    };
+        "Nepodarilo se rozdelit procesy."};
 
     fprintf(stderr, "%s\n", messages[errNum - 1]);
     exit(1);
@@ -37,7 +35,8 @@ int paramControl(int argc, char *argv[], parameters *params)
         errors(1);
     }
     char *endptrs[4];
-    for (int i=0; i<4;i++){
+    for (int i = 0; i < 4; i++)
+    {
         endptrs[i] = '\0';
     }
 
@@ -45,24 +44,29 @@ int paramControl(int argc, char *argv[], parameters *params)
     params->nh = strtol(argv[2], &endptrs[1], 10);
     params->ti = strtol(argv[3], &endptrs[2], 10);
     params->tb = strtol(argv[4], &endptrs[3], 10);
-    for (int i=0; i<4;i++){
-        if (endptrs[i][0] != '\0'){
+    for (int i = 0; i < 4; i++)
+    {
+        if (endptrs[i][0] != '\0')
+        {
             errors(2);
         }
     }
     if (params->no < 0 || params->nh < 0 || params->tb < 0 || params->tb > TB ||
-        params->ti < 0 || params->ti > TI){
-        errors(3);        
+        params->ti < 0 || params->ti > TI)
+    {
+        errors(3);
     }
     return 0;
 }
 
-void oxygen(){
+void oxygen()
+{
     printf("oxygen\n");
     exit(0);
 }
 
-void hydrogen(){
+void hydrogen()
+{
     printf("hydrogen\n");
     exit(0);
 }
@@ -71,25 +75,31 @@ int main(int argc, char *argv[])
 {
     parameters params;
     paramControl(argc, argv, &params);
-    int *m = mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    int *m = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *m = 10;
-    
-    for (int i=0; i<params.no;i++){
+
+    for (int i = 0; i < params.no; i++)
+    {
         pid_t process = fork();
-        if (process == 0){
+        if (process == 0)
+        {
             oxygen();
         }
-        else if (process == -1){
+        else if (process == -1)
+        {
             errors(4);
         }
     }
 
-    for (int i=0; i<params.nh;i++){
+    for (int i = 0; i < params.nh; i++)
+    {
         pid_t process = fork();
-        if (process == 0){
+        if (process == 0)
+        {
             hydrogen();
         }
-        else if (process == -1){
+        else if (process == -1)
+        {
             errors(4);
         }
     }
